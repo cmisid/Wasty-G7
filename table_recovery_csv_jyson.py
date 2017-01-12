49dff184c058c5479691fe124c463cd524bffbb7
Nicolas Paillier <nicolas.paillier@live.fr>
2017-01-12 15:56:32 +0100

    Génération dela table "Recovery" ; version 2 Modifications : - Importation des fichiers au format .csv en json - Ajout de la récupération de l'attribut "avert_state" - Ajout de la récupértion de l'attribut "sub_categ" - Ajout d'une boucle for pour récupérer les identifiants des catégories à partir des libellés des sous-catégories - dans la version 1, la variable "since" était générée aléatoirement ; désormais, nous avons utilisé la date de mise en ligne de l'annonce - dans la version 1, la variable "until" dépendait uniquement de la catégorie de l'objet ; désormais, elle dépend à la fois de la catégorie et du statut de l'annonce - Pour la génération finale de la table : l'identifiant de récupération est généré aléatoirement, l'identifiant de l'annonce est tiré aléatoirement à partir de la table "Advert", la date de récupération est générée en fonction de l'identifiant de l'annonce tout en respectant les contraintes exposées précédemment, l'identifiant de l'utilisateur qui récupère l'objet est généré aléatoirement à partir de la table utilisateur - Génération de 2000 lignes au lieu de 50 - Suppression de la génération de la création du fichier .csv

--- table_recovery_csv_jyson.py
+++ table_recovery_csv_jyson.py
@@ -10,70 +10,108 @@ import json
 from random import randrange
 from datetime import date, datetime, timedelta
 import pandas as pd
+import csv, math
 
-# Generation d'une liste dans laquelle il y a toutes les dates du 1 octobre 
-# 2016 à dans 30 jours à compter d'aujourd'hui.
-# Pour prendre en compte la condition des 30 jours d'une annonce en ligne.
+"""
+Ici on récupère l'attribut id_users de la table users
+Attribut nécéssaire pour l'élaboration de la table finale
+Pour faire cela, on utilise le fichier user.json précédemment généré
+"""
 
+fichier_users_json = pd.read_json('users.json', sep = ';') ####### bon titre de document par ce que pas encore de fichier de données
+fichier_users_json_trie = fichier_users_json[['id_users']]
 
-# "Since" correspond à la date : 1er octobre 2016 qui est la date de départ 
-# pour les générations de date
-since = datetime(year=2016, month=10, day=1)
+"""
+Ici on récupère les attributs id_advert, advert_datetime et sub_category de la table advert
+Attributs nécéssaire soit pour l'élaboration de la table finale, soit pour une meilleure génération de données
+Pour faire cela, on utilise le fichier advert.json précédemment généré
+fichier_advert_json : tout le fichier json, avec toutes les informations
+fichier_advert_json_trie : le fichier json qu'avec les attributs que l'on veut utiliser
+"""
 
-# "Until" correspond à la date d'aujourd'hui + 30 jours pour prendre en compte 
-# les délais de mise en ligne des annonces 
-until = datetime.now() + timedelta(days=30)
+fichier_advert_json = pd.read_json('advert.json', sep = ';') ####### bon titre de document par ce que pas encore de fichier de données
+fichier_advert_json_trie = fichier_advert_json[['id_advert','advert_datetime', 'sub_category', 'advert_state']]
 
-# Création de la variable date_range sous forme de liste 
-# vide à l'initialisation
-date_range = []
+# Boucle for pour récupérer les identifiants des catégories à partir des libellés des sous-catégories
+for i in range(len(tab_advert_json_trie)):
+	if libelle_souscateg == 'chaises/tabourets' or 'tables' or 'fauteuil' or 'canape' or 'literie' or 'armoire' or 'etageres' :
+		id_categ = 1
+	if libelle_souscateg == 'tapisseries' or 'tapis' or 'luminaires' or 'objets' :
+		id_categ = 2
+	if libelle_souscateg == 'outils' or 'accessoires' :
+		id_categ = 3
+	if libelle_souscateg == 'verre' or 'bois' or 'bois/carton' or 'gravier/gravat' or 'carrelage' or 'acier' or 'plastique' :
+		id_categ = 4
+	if libelle_souscateg == 'electromenager'
+		id_categ = 5
+	if libelle_souscateg == 'cuisine' or 'menage' or 'autres' :
+		id_categ = 6
+	if libelle_souscateg == 'maison' or 'homme' or 'femme' or 'enfant' :
+		id_categ = 7
+	if libelle_souscateg == 'vaisselle'
+		id_categ = 8
+	if libelle_souscateg == 'velo' or 'skate' or 'snow' or 'roller' or 'ski' or 'trotinette' :
+		id_categ = 9
+	if libelle_souscateg == 'autres/divers' :
+		id_categ = 10
 
-# Création des dates et des heures de manière aléatoire
-# draft_date_list : ébauche de la liste contenant les dates
-# nécessaire pour la création de la liste finale
-draft_date_list = pd.date_range(since,until,freq='12H')
-# Transformation de la liste des dates en liste de caractère
-# nécessaire pour lancer Jyson.
-date_list = pd.Series(draft_date_list.format())
-    
-# Entrée : N, un entier tq la valeur correspond à un tirage d'échantillon.
-# Objectif de la fonction : Générer des données de la table recovery
-# de manière aléatoire.
-# Sortie : Données aléatoires de la table recovery/récupération. 
+"""
+"Since" correspond à la date référence : la date de la mise en ligne de l'annonce
+Informations nécéssaire pour les générations de date
+tab_since =  tab_advert_json_trie[['advert_datetime']]
+"""
 
-def recovery(N):
+"""
+Création de la table until pour stocker les valeurs
+"Until" correspond à la date de mise en ligne + nombre de jours estimés par catégorie 
+recupérer la colonne état dans annonce. 
+"""
+
+tab_recov_date = []
+
+"""
+Ici on génère des dates de récupération d'objet selon leur état (en ligne, récupéré...) 
+Si l'objet est en ligne, on détermine une date de récupération selon le type de catégorie
+Tout en respectant le délais de 30 jours maximum de mise en ligne d'une annonce
+tab_transition : tableau qui contient une date de récupération
+"""
+for i in range(len(tab_advert_json_trie)):
+	if tab_advert_json_trie[['advert_state']] == 'en ligne' :
+		if tab_advert_json_trie[2][i] == 1 or tab_advert_json_trie[2][i] == 7 : # vérifier le numéro de colonne 
+			tab_recov_date = [][i] = tab_since[i] + timedelta(days = 15)
+		if tab_advert_json_trie[2][i] == 2 :
+			tab_recov_date = [][i] = tab_since[i] + timedelta(days = 25)
+		if tab_advert_json_trie[2][i] == 8 or tab_advert_json_trie[2][i] == 10 :
+			tab_recov_date = [][i] = tab_since[i] + timedelta(days = 30)
+		else :
+			tab_recov_date = [][i] = tab_since[i] + timedelta(days = 20)
+	else :
+		tab_recov_date = [][i] = NULL		
+
+"""
+Entrée : N, un entier tq la valeur correspond à un tirage d'échantillon.
+Objectif de la fonction : Générer des données de la table recovery
+de manière aléatoire.
+Sortie : Données aléatoires de la table recovery/récupération. 
+"""
+
+def recovery(N) :
 	data = [] 
 	z = {}                                                  
-	for i in range(N):
-		dl = choice(date_list)
-		z = {'id_recovery' : i, 'recovery_date' : dl, 'id_advert' : i, 'recovery_user' : i}
+	for i in range(N) :
+		for j in range(len(tab_advert_json_trie)) :
+			for k in range(len(fichier_users_json_trie)) :
+		dr = tab_recov_date
+        a = tab_advert_json_trie
+		z = {'id_recovery' : i, 'recovery_datetime' : dr[j], 'advert' : a[j], 'recovery_user' : k}
 		data.append(z)
 	return data
 
-# Génération des 50 données aléatoires
-random_data = recovery(50)
+# Génération des 2000 données aléatoires
+random_data = recovery(2000)
 
 # Création un fichier Recovery/Récupération en JSON
 jsonData = json.dumps(random_data)
 print(jsonData)
-with open ('C:/Users/Claire/Desktop/Recovery.json','w') as f:
+with open ('Recovery.json','w') as f:
 	json.dump(jsonData,f)
-    
-# Création d'un fichier Recovery.xls
-book = Workbook()
-table_recovery = book.add_sheet('recovery')
-table_recovery.write(0,0,"id_recovery")
-table_recovery.write(0,1,"recovery_date")
-table_recovery.write(0,2,"id_advert")
-table_recovery.write(0,3,"recovery_user")
-
-for i in range(1,len(random_data)):
-        table_recovery.write(i,0,random_data[i]["id_recovery"])
-        table_recovery.write(i,1,random_data[i]["recovery_date"])
-        table_recovery.write(i,2,random_data[i]["id_advert"])
-        table_recovery.write(i,3,random_data[i]["recovery_user"])   
-
-# Créer un fichier csv
-pd.DataFrame(random_data)
-pd.DataFrame(random_data).to_csv('Recovery.csv')
-book.save('C:/Users/Claire/Desktop/Recovery.csv')
